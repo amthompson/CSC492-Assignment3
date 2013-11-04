db362de6abf29b572658626f4d48ef0530b67d29
amthompson <andrew.thompson@mines.sdsmt.edu>
2013-11-03 23:08:19 -0700

    Initial Commit

---
+++ src/edu/sdsmt/thompsonsamson/weatherapp/IListeners.java
@@ -0,0 +1,10 @@
+package edu.sdsmt.thompsonsamson.weatherapp;
+
+import edu.sdsmt.thompsonsamson.weatherapp.model.Forecast;
+import edu.sdsmt.thompsonsamson.weatherapp.model.ForecastLocation;
+
+public interface IListeners
+{
+	public void onLocationLoaded(ForecastLocation forecastLocation);
+	public void onForecastLoaded(Forecast forecast);
+}
